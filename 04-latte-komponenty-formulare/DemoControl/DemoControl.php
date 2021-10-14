6429d105d19bb9cafc2e86d698f666378d898124
Stanislav Vojíř <stanislav.vojir@gmail.com>
2021-10-15 00:57:31 +0200

    jednoduchá ukázka komponenty

---
+++ 04-latte-komponenty-formulare/DemoControl/DemoControl.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Components\DemoControl;
+
+use Nette\Application\UI\Control;
+
+/**
+ * Class DemoControl - ukázková, vlastně hrozně hloupá komponenta
+ * @package App\Components\DemoControl
+ */
+class DemoControl extends Control{
+
+  /** @var string $text */
+  public $text = 'DEMO';
+
+  public function render():void {
+    $this->template->text = $this->text;
+    $this->template->render(__DIR__.'/default.latte');
+  }
+
+  public function renderHello(string $message):void {
+    $this->template->text=$this->text;
+    $this->template->message=$message;
+    $this->template->render(__DIR__.'/hello.latte');
+  }
+  
+}
