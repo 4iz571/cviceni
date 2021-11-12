da9397466000c2c827cb905bb08694a2b799c898
Stanislav Vojíř <stanislav.vojir@gmail.com>
2021-11-12 01:28:00 +0100

    notes4 - příprava pro obnovu zapomenutého hesla

--- 07-fb-uzivatele/notes4/app/Model/Entities/User.php
+++ 07-fb-uzivatele/notes4/app/Model/Entities/User.php
@@ -9,7 +9,7 @@ use LeanMapper\Entity;
  * @package App\Model\Entities
  * @property int $userId
  * @property string $name
- * @property Role $role m:hasOne
+ * @property Role|null $role m:hasOne
  * @property string $email
  * @property string $password
  */
