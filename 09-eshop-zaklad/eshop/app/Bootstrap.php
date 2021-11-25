9531a56cdda5a3ef21b247cc9f9f78f7aeb38fe5
Stanislav Vojíř <stanislav.vojir@gmail.com>
2021-11-25 22:57:03 +0100

    základ aplikace

--- 09-eshop-zaklad/eshop/app/Bootstrap.php
+++ 09-eshop-zaklad/eshop/app/Bootstrap.php
@@ -15,7 +15,7 @@ class Bootstrap{
 		$configurator = new Configurator;
 		$appDir = dirname(__DIR__);
 
-		$configurator->setDebugMode(false); //XXX
+		$configurator->setDebugMode(true); //XXX
 		$configurator->enableTracy($appDir . '/log');
 
 		$configurator->setTimeZone('Europe/Prague');
