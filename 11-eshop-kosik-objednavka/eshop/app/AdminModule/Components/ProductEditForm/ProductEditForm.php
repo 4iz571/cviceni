685e537c010a44058179bb067c9a541ca408854b
Stanislav Vojíř <stanislav.vojir@gmail.com>
2022-11-28 12:37:26 +0100

    Update ProductEditForm.php

--- 11-eshop-kosik-objednavka/eshop/app/AdminModule/Components/ProductEditForm/ProductEditForm.php
+++ 11-eshop-kosik-objednavka/eshop/app/AdminModule/Components/ProductEditForm/ProductEditForm.php
@@ -86,7 +86,7 @@ class ProductEditForm extends Form{
 
     $this->addText('price', 'Cena')
       ->setHtmlType('number')
-      ->addRule(Form::NUMERIC)
+      ->addRule(Form::NUMERIC,'Musíte zadat platnou cenu produktu')
       ->setRequired('Musíte zadat cenu produktu');//tady by mohly být další kontroly pro min, max atp.
 
     $this->addCheckbox('available', 'Nabízeno ke koupi')
