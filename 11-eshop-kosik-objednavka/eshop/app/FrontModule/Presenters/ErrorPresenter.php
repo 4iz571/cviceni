a0fc3162662e0f9dd9b6bed36526c1123409029f
Stanislav Vojíř <stanislav.vojir@gmail.com>
2022-12-05 01:05:46 +0100

    aktualizace z podkladů 10

--- 11-eshop-kosik-objednavka/eshop/app/FrontModule/Presenters/ErrorPresenter.php
+++ 11-eshop-kosik-objednavka/eshop/app/FrontModule/Presenters/ErrorPresenter.php
@@ -12,27 +12,22 @@ use Tracy\ILogger;
 final class ErrorPresenter implements Nette\Application\IPresenter{
 	use Nette\SmartObject;
 
-	/** @var ILogger */
-	private $logger;
+	private ILogger $logger;
 
-
-	public function __construct(ILogger $logger)
-	{
+	public function __construct(ILogger $logger){
 		$this->logger = $logger;
 	}
 
-
-	public function run(Nette\Application\Request $request): Nette\Application\Response
-	{
+	public function run(Nette\Application\Request $request): Nette\Application\Response{
 		$exception = $request->getParameter('exception');
 
-		if ($exception instanceof Nette\Application\BadRequestException) {
+		if ($exception instanceof Nette\Application\BadRequestException){
 			[$module, , $sep] = Nette\Application\Helpers::splitName($request->getPresenterName());
 			return new Responses\ForwardResponse($request->setPresenterName($module . $sep . 'Error4xx'));
 		}
 
 		$this->logger->log($exception, ILogger::EXCEPTION);
-		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
+		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse):void {
 			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
 				require __DIR__ . '/templates/Error/500.phtml';
 			}
