8410410535551139f3669941b3ccfa036c1a4927
Stanislav Vojíř <stanislav.vojir@gmail.com>
2023-11-13 11:25:57 +0100

    Update NoteEditForm.php

--- 08-acl-git-eshop-analyza/notes6/app/Components/NoteEditForm/NoteEditForm.php
+++ 08-acl-git-eshop-analyza/notes6/app/Components/NoteEditForm/NoteEditForm.php
@@ -47,7 +47,7 @@ class NoteEditForm extends Form{
    * @param Nette\ComponentModel\IContainer|null $parent
    * @param string|null $name
    */
-  pubic function __construct(Nette\Security\User $currentUser, CategoriesFacade $categoriesFacade, NotesFacade $notesFacade, UsersFacade $usersFacade, Nette\ComponentModel\IContainer $parent = null, string $name = null){
+  public function __construct(Nette\Security\User $currentUser, CategoriesFacade $categoriesFacade, NotesFacade $notesFacade, UsersFacade $usersFacade, Nette\ComponentModel\IContainer $parent = null, string $name = null){
     parent::__construct($parent, $name);
     $this->categoriesFacade=$categoriesFacade;
     $this->notesFacade=$notesFacade;
