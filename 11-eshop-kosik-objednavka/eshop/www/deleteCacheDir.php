dd30e756d631e699d170483adf4075d51fe7861b
Stanislav Vojíř <stanislav.vojir@gmail.com>
2024-12-05 22:49:53 +0100

    skript pro smazání cache, oprava configu

---
+++ 11-eshop-kosik-objednavka/eshop/www/deleteCacheDir.php
@@ -0,0 +1,26 @@
+<?php
+
+echo 'smazání cache';
+
+/**
+ * Funkce pro smazání adresáře včetně veškerého jeho obsahu
+ */
+function delete_directory($dirname) {
+  if (is_dir($dirname))
+    $dir_handle = opendir($dirname);
+  if (!$dir_handle)
+    return false;
+     
+  while($file = readdir($dir_handle)) {
+    if ($file != "." && $file != "..") {
+      if (!is_dir($dirname."/".$file))
+        unlink($dirname."/".$file);
+      else
+        delete_directory($dirname.'/'.$file);
+    }
+  }
+  closedir($dir_handle);
+  rmdir($dirname);
+}
+
+delete_directory(__DIR__.'/../temp/cache');
