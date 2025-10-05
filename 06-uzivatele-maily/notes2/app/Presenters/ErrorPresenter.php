dd2b14e87b5c50896f454b0b1defd80259308c1a
Stanislav Vojíř <stanislav.vojir@gmail.com>
2025-10-05 21:20:37 +0200

    reformat error presenterů

--- 06-uzivatele-maily/notes2/app/Presenters/ErrorPresenter.php
+++ 06-uzivatele-maily/notes2/app/Presenters/ErrorPresenter.php
@@ -9,31 +9,24 @@ use Nette\Application\Responses;
 use Nette\Http;
 use Tracy\ILogger;
 
-
 final class ErrorPresenter implements Nette\Application\IPresenter {
-	use Nette\SmartObject;
+  use Nette\SmartObject;
 
-	private ILogger $logger;
+  public function __construct(public ILogger $logger) {}
 
+  public function run(Nette\Application\Request $request): Nette\Application\Response {
+    $exception=$request->getParameter('exception');
 
-	public function __construct(ILogger $logger){
-		$this->logger = $logger;
-	}
+    if ($exception instanceof Nette\Application\BadRequestException) {
+      [$module, , $sep]=Nette\Application\Helpers::splitName($request->getPresenterName());
+      return new Responses\ForwardResponse($request->setPresenterName($module.$sep.'Error4xx'));
+    }
 
-
-	public function run(Nette\Application\Request $request):Nette\Application\Response {
-		$exception = $request->getParameter('exception');
-
-		if ($exception instanceof Nette\Application\BadRequestException) {
-			[$module, , $sep] = Nette\Application\Helpers::splitName($request->getPresenterName());
-			return new Responses\ForwardResponse($request->setPresenterName($module . $sep . 'Error4xx'));
-		}
-
-		$this->logger->log($exception, ILogger::EXCEPTION);
-		return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
-			if (preg_match('#^text/html(?:;|$)#', (string) $httpResponse->getHeader('Content-Type'))) {
-				require __DIR__ . '/templates/Error/500.phtml';
-			}
-		});
-	}
+    $this->logger->log($exception, ILogger::EXCEPTION);
+    return new Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse): void {
+      if (preg_match('#^text/html(?:;|$)#', (string)$httpResponse->getHeader('Content-Type'))) {
+        require __DIR__.'/templates/Error/500.phtml';
+      }
+    });
+  }
 }
